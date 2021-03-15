7781bedb6d1c073eb8c093285af6899c9cad7e8f
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2021-03-15 02:35:19 -0700

    luis

--- obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,70 +13,70 @@ namespace Tarea_7.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\USER\Documents\GitHub\Tarea-7\_Imports.razor"
+#line 1 "D:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\USER\Documents\GitHub\Tarea-7\_Imports.razor"
+#line 2 "D:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\USER\Documents\GitHub\Tarea-7\_Imports.razor"
+#line 3 "D:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\USER\Documents\GitHub\Tarea-7\_Imports.razor"
+#line 4 "D:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\USER\Documents\GitHub\Tarea-7\_Imports.razor"
+#line 5 "D:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\USER\Documents\GitHub\Tarea-7\_Imports.razor"
+#line 6 "D:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\USER\Documents\GitHub\Tarea-7\_Imports.razor"
+#line 7 "D:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\USER\Documents\GitHub\Tarea-7\_Imports.razor"
+#line 8 "D:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\USER\Documents\GitHub\Tarea-7\_Imports.razor"
+#line 9 "D:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
 using Tarea_7;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\USER\Documents\GitHub\Tarea-7\_Imports.razor"
+#line 10 "D:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
 using Tarea_7.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using Tarea_7.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 43 "C:\Users\USER\Documents\GitHub\Tarea-7\Shared\NavMenu.razor"
+#line 43 "D:\Clases\Programacion 3\Tarea 7\Tarea-7\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
