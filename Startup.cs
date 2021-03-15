61c53836079226453f18b2e106a6a6b3de5ea5fc
Alejandro <ageldev@outlook.com>
2021-03-15 16:03:37 -0400

    Adelanto de Registro, Clase Persona (El nombre puede cambiar), Validacion del zodiaco.

--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,7 @@ namespace Tarea_7
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
