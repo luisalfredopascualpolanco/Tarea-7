6997ca3ff7a7c571611cd0ade9cee6c23ca2d06d
98Raymond25 <79771930+98Raymond25@users.noreply.github.com>
2021-03-15 01:02:38 -0400

    Anular registro terminado

--- obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
@@ -13,70 +13,70 @@ namespace Tarea_7
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "d:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
+#line 1 "C:\Users\Ramon\Source\Repos\luisalfredopascualpolanco\Tarea-7\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "d:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
+#line 2 "C:\Users\Ramon\Source\Repos\luisalfredopascualpolanco\Tarea-7\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "d:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
+#line 3 "C:\Users\Ramon\Source\Repos\luisalfredopascualpolanco\Tarea-7\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "d:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
+#line 4 "C:\Users\Ramon\Source\Repos\luisalfredopascualpolanco\Tarea-7\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "d:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
+#line 5 "C:\Users\Ramon\Source\Repos\luisalfredopascualpolanco\Tarea-7\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "d:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
+#line 6 "C:\Users\Ramon\Source\Repos\luisalfredopascualpolanco\Tarea-7\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "d:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
+#line 7 "C:\Users\Ramon\Source\Repos\luisalfredopascualpolanco\Tarea-7\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "d:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
+#line 8 "C:\Users\Ramon\Source\Repos\luisalfredopascualpolanco\Tarea-7\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "d:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
+#line 9 "C:\Users\Ramon\Source\Repos\luisalfredopascualpolanco\Tarea-7\_Imports.razor"
 using Tarea_7;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "d:\Clases\Programacion 3\Tarea 7\Tarea-7\_Imports.razor"
+#line 10 "C:\Users\Ramon\Source\Repos\luisalfredopascualpolanco\Tarea-7\_Imports.razor"
 using Tarea_7.Shared;
 
 #line default
